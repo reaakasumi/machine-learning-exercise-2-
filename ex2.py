5908a857041e4f662e380e01eaf979d9d0bb7c67
Hrithik Sharma <e12023164@student.tuwien.ac.at>
2024-05-25 15:45:14 +0200

    knn added metrics

--- ex2.py
+++ ex2.py
@@ -2,21 +2,29 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import SGDRegressor
 from gradient_descent import gradient_descent
+from simp_knn import custom_knn
 from sklearn.preprocessing import MinMaxScaler
 
 #preprocessing
-df = pd.read_csv("Student_Performance.csv").head(1000)
-df.insert(0, "b", 1) #yx + b
+df = pd.read_csv("Student_Performance.csv").head(100)
+#df.insert(0, "b", 1) #yx + b
 features = df. loc[:, df. columns != "Performance Index"]
 target = df. loc[:, df. columns == "Performance Index"]
 features['Extracurricular Activities'] = features['Extracurricular Activities'].map({'Yes': 1, 'No': 0})
 
 scalar = MinMaxScaler()
 features = scalar.fit_transform(features)
-features[::, 0] = 1.
+#features[::, 0] = 1.
 
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.25, random_state=2)
 
+model = custom_knn()
+model.fit(X_train,y_train, 2)
+
+pred = model.predict(X_test, 3)
+sk_result = pd.DataFrame({'Actual': y_test.values.flatten(), 'Predicted': pred.flatten()})
+print(sk_result)
+""" 
 model = gradient_descent()
 model.fit(X_train,y_train)
 pred = model.predict(X_test)
@@ -31,3 +39,5 @@ sk_result = pd.DataFrame({'Actual': y_test.values.flatten(), 'Predicted': sk_mod
 print(sk_result)
 
 #result -> both are similar!
+
+"""
---
+++ simp_knn.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import cosine_distances
+from sklearn.metrics.pairwise import manhattan_distances
+
+
+class custom_knn:
+    def __init__(self):
+        self.k = None
+        self.x_train = None
+        self.y_train = None
+
+    #distance metrics:
+    #1 = euclician
+    #2 = cosine_distance
+    #3 = manhattan_distance
+    def fit(self, x_train, y_train, k = 5):
+        self.k = k
+        self.x_train = x_train
+        self.y_train = y_train.to_numpy()
+
+    def predict(self, x_test, distance_metric = 1):
+        if(distance_metric == 1):
+            distances = euclidean_distances(x_test, self.x_train)
+        elif(distance_metric == 2):
+            distances = cosine_distances(x_test, self.x_train)
+        elif(distance_metric == 3):
+            distances = manhattan_distances(x_test, self.x_train)
+        else:
+            print("Invalid distance metric")
+            return
+        k_indices = np.argsort(distances, axis=1)[:, :self.k]
+
+        return np.mean(self.y_train[k_indices], axis=1)
+
